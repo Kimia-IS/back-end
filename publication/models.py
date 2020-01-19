4e7fe0418f6d35f03d9df20ef65035de726cb145
Vincent Siauw <jamesvincentsiauw@gmail.com>
2020-01-20 03:36:19 +0700

    publication CRUD function complete

--- publication/models.py
+++ publication/models.py
@@ -42,7 +42,7 @@ class journal(db.Model):
             else:
                 ret = {
                     'status': 200,
-                    'message': 'Your jornal already registered before, please try again another journal!'
+                    'message': 'Your journal already registered before, please try again another journal!'
                 }
             return ret
         except Exception as e:
@@ -98,8 +98,8 @@ class journal_corresponding_author(db.Model):
 
 class patent(db.Model):
     id = db.Column(db.Integer, unique=True, primary_key=True)
-    title = db.Column(db.String(255), unique=True)
     lecturer_nip = db.Column(db.String(255), ForeignKey('lecturer.nip'), unique=False)
+    title = db.Column(db.String(255), unique=True)
     status = db.Column(db.String(255), unique=False)
     publisher = db.Column(db.String(255), unique=False)
     year = db.Column(db.String(255), unique=False)
@@ -107,32 +107,29 @@ class patent(db.Model):
 
     def save(self):
         try:
-            check_journal = sess.query(journal).filter(journal.lecturer_nip == self.lecturer_nip).\
-                filter(journal.title == self.title).first()
-            if check_journal is None:
+            check_patent = sess.query(patent).filter(patent.lecturer_nip == self.lecturer_nip).\
+                filter(patent.title == self.title).first()
+            if check_patent is None:
                 sess.add(self)
                 sess.commit()
-                new_journal = {
+                new_patent = {
                     'id': self.id,
                     'lecturer_nip': self.lecturer_nip,
                     'title': self.title,
-                    'issue': self.issue,
+                    'status': self.status,
                     'year': self.year,
-                    'total_page': self.total_page,
-                    'type': self.type,
-                    'doi': self.doi,
-                    'link': self.link,
+                    'publisher': self.publisher,
                     'filepath': self.filepath
                 }
                 ret = {
                     'status': 200,
-                    'message': 'New Journal Registered',
-                    'results': new_journal
+                    'message': 'New Patent Registered',
+                    'results': new_patent
                 }
             else:
                 ret = {
                     'status': 200,
-                    'message': 'Your jornal already registered before, please try again another journal!'
+                    'message': 'Your patent already registered before, please try again another patent!'
                 }
             return ret
         except Exception as e:
@@ -142,6 +139,7 @@ class patent(db.Model):
             }
             return ret
 
+
 class other_publication(db.Model):
     id = db.Column(db.Integer, unique=True, primary_key=True)
     title = db.Column(db.String(255), unique=True)
@@ -149,3 +147,154 @@ class other_publication(db.Model):
     date = db.Column(db.Date, unique=False)
     publisher = db.Column(db.String(255), unique=False)
     filepath = db.Column(db.String(255), unique=False)
+
+    def save(self):
+        try:
+            check_others = sess.query(other_publication).filter(other_publication.lecturer_nip == self.lecturer_nip).\
+                filter(other_publication.title == self.title).first()
+            if check_others is None:
+                sess.add(self)
+                sess.commit()
+                new_other_publication = {
+                    'id': self.id,
+                    'lecturer_nip': self.lecturer_nip,
+                    'title': self.title,
+                    'date': self.date,
+                    'publisher': self.publisher,
+                    'filepath': self.filepath
+                }
+                ret = {
+                    'status': 200,
+                    'message': 'New Publication Registered',
+                    'results': new_other_publication
+                }
+            else:
+                ret = {
+                    'status': 200,
+                    'message': 'Your publication already registered before, please try again another publication!'
+                }
+            return ret
+        except Exception as e:
+            ret ={
+                'status': 200,
+                'message': e.args
+            }
+            return ret
+
+
+def get_all_publication():
+    try:
+        journals = sess.query(journal, journal_corresponding_author).filter(journal.id == journal_corresponding_author.journal_id).all()
+        patents = sess.query(patent).all()
+        others = sess.query(other_publication).all()
+
+        res = {
+            'status': 200,
+            'message': 'These are the registered publications',
+            'results': {
+                'journals': journals,
+                'patents': patents,
+                'other_publications': others
+            }
+        }
+
+        return res
+    except Exception as e:
+        res = {
+            'status': 200,
+            'message': e.args
+        }
+
+        return res
+
+
+def get_publication_byID(cat, id):
+    try:
+        selected_publication = sess.query(cat).filter(cat.id==id).first()
+        if cat == 'journal':
+            corresponding_author = sess.query(journal_corresponding_author).filter(journal_corresponding_author.journal_id == id).first()
+            res ={
+                'status': 200,
+                'message': 'This is the requested publication',
+                'result': {
+                    'category': 'journal',
+                    'publication': selected_publication,
+                    'corresponding_author': ast.literal_eval(corresponding_author.names)
+                }
+            }
+        else:
+            res = {
+                'status': 200,
+                'message': 'This is the requested publication',
+                'result': {
+                    'category': cat,
+                    'publication': selected_publication
+                }
+            }
+        return res
+    except Exception as e:
+        ret = {
+            'status': 200,
+            'message': e.args,
+        }
+        return ret
+
+
+def edit_publication(cat, id, request):
+    try:
+        selected_publication = sess.query(cat).filter(cat.id == id).first()
+        if selected_publication is not None:
+            data = {}
+            for k in request.keys():
+                param = k
+                data[k] = request[param]
+            edit = sess.query(cat).filter(cat.id == id).update(data, synchronize_session=False)
+            sess.commit()
+            if edit == 1:
+                ret = {
+                    'status': 200,
+                    'message': 'Data updated!'
+                }
+            else:
+                ret = {
+                    'status': 500,
+                    'message': "Something's went wrong with our server. Please try again later!"
+                }
+            return ret
+        else:
+            ret = {
+                'status': 200,
+                'message': "Publication is not registered"
+            }
+            return ret
+    except Exception as e:
+        ret = {
+            'status': 200,
+            'message': e.args,
+        }
+        return ret
+
+
+def delete_experience(cat, id):
+    try:
+        selected_publication = sess.query(cat).filter(cat.id == id).first()
+        if selected_publication is not None:
+            sess.delete(selected_publication)
+            sess.commit()
+            ret = {
+                'status': 200,
+                'message': 'Data deleted!'
+            }
+            return ret
+        else:
+            ret = {
+                'status': 200,
+                'message': "Publication is not registered"
+            }
+            return ret
+    except Exception as e:
+        ret = {
+            'status': 200,
+            'message': e.args
+        }
+        return ret
